f5098bb7ab2f5f44cb3952cc8795d86785b77db1
Rick Companje @ HUA <r.companje@hetutrechtsarchief.nl>
2019-07-16 16:20:00 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Het Utrechts Archief - Chat experiment</title>
+  <style>
+    body { 
+      background-color: #f5f5f5;
+      font-family: Roboto, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      margin: 0px;
+    }
+    h1 {
+      font-weight: normal;
+    }
+    li { 
+      line-height: 30px;
+      margin-bottom: 30px;
+    }
+    p {
+      line-height: 30px;
+    }
+    h2 {
+      margin-top: 30px;
+    }
+    a:link, a:visited {
+      color: black;
+    }
+    #header {
+      height: 60px;
+      background-color: white;
+    }
+
+    .content {
+      max-width: 700px;
+      margin: auto;
+    }
+
+    #logo {
+      float: right;
+      width: 100px;
+      margin-top: -70px;
+    }
+    
+    .footer, .footer p {
+      text-align: center;
+      line-height: 11px;
+    }
+    
+  </style>
+</head>
+
+<body>
+
+<div id="header"></div>
+<div class="content">
+  <img id="logo" src="hualogo.png">
+  <h1>Chat experiment Het Utrechts Archief</h1>
+  <p>Team dienstverlening gaat in het najaar chatten met publiek via de website. Tijdens de zomerperiode willen we de tool intern uittesten en oefenen met het chatten. Help je mee door ons een vraag te stellen? Dat kan van alles zijn. Bijvoorbeeld wat de status is van een bepaald verzoek, en of we een kopje koffie lusten :) </p>
+  <p>Het werkt zo:</p>
+  <ol>
+    <li>Sleep dit icoon naar de favorieten balk van je browser en plaats hem erin;
+    <div>
+     <a style="background-color:#3fbfad;font-family:Arial;color:white;border-radius:3px;padding:4px;margin-left:5px"
+       href="javascript:(function()%7B%24(%22.popup-container%22).remove()%3B%24(%20'%3Cdiv%20class%3D%22popup-container%22%3E%3Ciframe%20src%3D%22https%3A%2F%2Fhualab.nl%2Fchat%2Fonline.html%3F'%2BMath.random()%2B'%22%20width%3D%22370%22%20height%3D%22210%22%3E%3C%2Fiframe%3E%3C%2Fdiv%3E'%20).insertAfter(%20%22%23menu-main%22%20)%3B%24(%22.scroll-help__button%22).hide()%7D)()">
+       Chat
+     </a>
+    </div>
+    </li>
+    <li>Ga naar onze site <a target="_blank" href="https://www.hetutrechtsarchief.nl">www.hetutrechtsarchief.nl</a> en klik op het icoon dat je net naar je favorieten hebt gesleept. Het werkt alleen als je er op klikt <strong>vanaf</strong> onze site.</li>
+    <li>Rechts beneden in je scherm zie je de chat pop-up. Het werkt ook alleen als je erop klikt vanaf onze site. Klik erop. Je ziet wie er op dat moment online is. </li>
+    <li>Stel je vraag! </li>
+  </ol>
+
+  <div class="footer">
+    <p>Wat vond je ervan? Laat het ons weten, via de chat natuurlijk :) </p>
+    <p>Hartelijke groet,</p>
+    <p>Team dienstverlening</p>
+    <h2>Lukt het niet of heb je vragen? Bel Hannah: 030 286 66 81</h2>
+  </div>
+</div>
+
+</body>
+</html>
+
---
+++ status.php
@@ -0,0 +1,89 @@
+<?php
+
+// available,chat
+// away,dnd,xa,
+// unavailable
+
+$usernames = [ "hjansen", "mieke", "r.companje", "anna", "mustafa", "reinier" ];
+shuffle($usernames);
+$users = [];
+foreach  ($usernames as $username) {
+  $user = [];
+  $user["name"] = $username;
+  $user["status"]= file_get_contents("https://eu.libraryh3lp.com/presence/jid/$username/eu.libraryh3lp.com/text");
+  $users[] = $user;
+}
+
+$current_username = "";
+$current_status = "offline";
+
+
+//first check 'away's
+foreach ($users as $user) {
+  if ($user["status"]=="away" || $user["status"]=="dnd" || $user["status"]=="xa") {
+    $current_username = $user["name"];
+    $current_status = "away";
+  }
+}
+
+//if there's an 'available' user that one gets priority
+foreach ($users as $user) {
+  if ($user["status"]=="available" || $user["status"]=="chat") {
+    $current_username = $user["name"];
+    $current_status = "online";
+  }
+}
+
+if ($_SERVER['QUERY_STRING']=='debug') {
+  header('Content-Type: application/json');
+  echo json_encode($users);
+  die();
+}
+
+if ($_SERVER['QUERY_STRING']=='json') {
+  header('Content-Type: application/json');
+  $obj = (object)['user'=>$current_username, 'status'=>$current_status];
+  echo json_encode($obj);
+  die();
+}
+
+// if ($_SERVER['QUERY_STRING']=='image') {
+  header('Content-Type: image/png');
+
+  if ($current_status=="offline") {
+    //when everyone is offline we can't show a user's icon and name  
+    imagepng(imagecreatefrompng("img/status/offline.png"));
+  } else if ($current_status=="away") {
+    imagepng(imagecreatefrompng("img/status/away.png"));
+  } else {
+    //when at least one person is online or away we can show that user's icon and name
+    $icon = imagecreatefrompng("img/icons/$current_username.png");
+    $name = imagecreatefrompng("img/names/$current_username.png");
+    $status = imagecreatefrompng("img/status/$current_status.png");
+    $dest = imagecreatetruecolor(imagesx($icon)+imagesx($name)+imagesx($status), 60); 
+
+    imagecopy($dest, $icon, 
+      /*dst xy:*/ 0, 0, 
+      /*src xy:*/ 0, 0,  
+      /*src wh:*/ 60, 60); 
+
+    imagecopy($dest, $name, 
+      /*dst xy:*/ 60, 0, 
+      /*src xy:*/ 0, 0,  
+      /*src wh:*/ imagesx($name), 60); 
+
+    imagecopy($dest, $status, 
+      /*dst xy:*/ 60 + imagesx($name), 0, 
+      /*src xy:*/ 0, 0,  
+      /*src wh:*/ imagesx($name) + imagesx($status), 60); 
+
+    header('Content-Type: image/png');
+    imagepng($dest);
+  }
+
+// } else { //show as text
+//   if ($current_status!="online") echo($current_status);
+//   else echo "$current_username: $current_status";
+// }
+
+?>

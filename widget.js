b7bab1aa3114ab11a9e9352f66ed9a9e7e3ed073
Rick Companje @ HUA <r.companje@hetutrechtsarchief.nl>
2019-10-24 16:46:42 +0200

    getting ready for the release

--- widget.js
+++ widget.js
@@ -1,11 +1,16 @@
-// $("body").append('<div style="position:fixed;right:0px;bottom:0px;background-color:red">XX</div>');
-// style="position:absolute;left:0px;top:0px" class="btnIcon" id="btnChat"><img src="https://hualab.nl/chat/img/icon-chat.png?"></div>');
+$(document).ready(function() {
+  $('<div class="chat" style="display: none; position:fixed; bottom: 0; right:20px; z-index:600; width: 370px"><iframe src="https://hualab.nl/chat/online.html?' + Math.random() + '" width="370" height="210"></iframe></div>').insertAfter("#topbar");
+  $(".scroll-help__button").hide()
+  $(".popup-container").css("left", "20px");
+  $(".popup-container .toggle").css("margin-left", "0px");
+  $(".popup-container .toggle").css("border-top", "2px solid #3fbfad");
+  $(".popup-container .toggle").css("border-left", "2px solid #3fbfad");
+  $(".popup-container .toggle").css("border-right", "2px solid #3fbfad");
+  $(".popup-container .toggle").css("border-radius", "5px");
 
-$('<div class="chat" style="position:fixed; bottom: 0; right:20px; z-index:600; width: 370px"><iframe src="https://hualab.nl/chat/online.html?' + Math.random() + '" width="370" height="210"></iframe></div>').insertAfter("#topbar");
-$(".scroll-help__button").hide()
-$(".popup-container").css("left", "20px");
-$(".popup-container .toggle").css("margin-left", "0px");
-$(".popup-container .toggle").css("border-top", "2px solid #3fbfad");
-$(".popup-container .toggle").css("border-left", "2px solid #3fbfad");
-$(".popup-container .toggle").css("border-right", "2px solid #3fbfad");
-$(".popup-container .toggle").css("border-radius", "5px");
+  var isSmallScreen = window.matchMedia('(max-width: 800px)').matches;
+
+  if (!isSmallScreen) {
+    $("div.chat").show();
+  }
+});

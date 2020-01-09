ee8d8ecb1231beca5ae5c89f5076e77e30f9fd8e
Rick Companje <rick@companje.nl>
2020-01-09 17:23:54 +0100

    poging event send google tag manager

--- widget.js
+++ widget.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
-  $('<div class="chat" style="display: none; position:fixed; bottom: 0; right:20px; z-index:600; width: 370px"><iframe src="https://hualab.nl/chat/online.html?' + Math.random() + '" width="370" height="210"></iframe></div>').insertAfter("#topbar");
+  $('<div class="chat" style="display: none; position:fixed; bottom: 0px; right:20px; z-index:600; width: 370px"><iframe src="https://hualab.nl/chat/online.html?' + Math.random() + '" width="370" height="210"></iframe></div>').insertAfter("#topbar");
   $(".scroll-help__button").hide()
   $(".popup-container").css("left", "20px");
+  // $(".popup-container").css("width", "80%");
   $(".popup-container .toggle").css("margin-left", "0px");
   $(".popup-container .toggle").css("border-top", "2px solid #3fbfad");
   $(".popup-container .toggle").css("border-left", "2px solid #3fbfad");
